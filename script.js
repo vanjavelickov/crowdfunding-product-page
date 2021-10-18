037a342dc8b22cd0c2c2051e4eab41feb93e099f
vanja velickov <vanjavelickov@gmail.com>
2021-10-18 13:15:29 +0200

    adding anchor tag and style to btn secondary

---
+++ script.js
@@ -0,0 +1,12 @@
+let bookmarkedBtn = document.getElementById('bookmarkedBtn');
+function bookmarked(){
+    let buttonSecondary = document.getElementsByClassName('btnSecondary');
+    buttonSecondary[0].style.color = 'hsl(176, 72%, 28%)';
+    let bookmarkedText = document.getElementById('bookmarked');
+    // let bookmarkedIcon = document.getElementById('bookmarkIcon');
+    // bookmarkedIcon.style.opacity = '0.6';
+    // bookmarkedIcon.style.borderRadius = '50%';
+    // bookmarkedIcon.style.mixBlendMode = 'color';
+    // bookmarkedIcon.style.color = 'red';
+    bookmarkedText.innerHTML = 'Bookmarked';
+}
